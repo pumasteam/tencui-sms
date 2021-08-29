71ec2e7bb8b1e30d35581f36a8dce148bcf71d66
SciDroid <scidroidgames@gmail.com>
2021-08-29 02:00:02 -0500

    no se si funcione pero hay que intentarlo

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 import json
+from pymongo import MongoClient
 
+DB = MongoClient("").tencui.tencui
 SID = "YOUR_SID"
 AUTH_TOKEN = "YOUR_AUTH_TOKEN"
 PHONE_NUMBER = "TWILIO_PHONE_NUMBER"
@@ -22,12 +24,10 @@ def add_notification():
             to = number
         )
 
-@app.route('/homework', methods = ['POST', 'GET'])
+@app.route('/get', methods = ['GET'])
 def send_hw():
-    homework_example = {
-        "tareas": ["Tarea de Ejemplo 1", "Tarea de Ejemplo 2", "Tarea de Ejemplo 3", "Tarea de Ejemplo 4"]
-        }
-    return homework_example
+    raw_data = list(db.find({}))
+    return json.dumps(raw_data)
 
 @app.route('/sms', methods = ['GET', 'POST'])
 def get_response():
@@ -42,6 +42,11 @@ def get_response():
     if message_body.lower() == 'tareas':
         response.message('Tus tareas son:' + tareas)
     return str(response)
+
+@app.route('/add', methods = ['POST'])
+def add_data():
+    request_data = request.get_json()
+    db.insert_one(request_data)
 
 if __name__ == '__main__':
     app.run(debug = True)
